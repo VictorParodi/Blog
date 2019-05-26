0fcb955d98442b9b4a72b004ef9702860ba2f545
VictorParodi <victor.parodi.montero@gmail.com>
2019-05-26 17:24:47 -0500

    master commit 1: initial setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="ui container grid">
+            <div className="row">
+                <div className="column sixteen wide">
+                    App Component
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
