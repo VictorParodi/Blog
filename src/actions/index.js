6a2159c35abc4638487b1b847c34905d3e636591
VictorParodi <victor.parodi.montero@gmail.com>
2019-05-26 20:02:47 -0500

    app1 commit 2: using Redux Thunk to use asynchronous action creators

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import jsonplaceholder from './../apis/jsonplaceholder.js';
+
+export const fetchPosts = () => {
+    return async (dispatch, getState) => {
+        const response = await jsonplaceholder.get('/posts');
+        dispatch({type: 'FETCH_POSTS', payload: response});
+    }
+};
